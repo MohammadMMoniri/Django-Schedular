0958dbd494b3601d83b1daabac0a9d68b94b47dd
mohammadmmoniri <mmmoniri77@gmail.com>
2024-01-25 13:59:06 +0330

    feat: user

--- schedular/user/models.py
+++ schedular/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, UserManager
 
-# Create your models here.
+USER_TYPE = (
+    ("a", "admin"),
+    ("n", "normal"),
+)
+
+class User(AbstractUser):
+    user_type = models.CharField(choices=USER_TYPE, max_length=1, default='a')
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
